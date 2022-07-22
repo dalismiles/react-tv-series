79dee74fe0c2471288f0147ad1e213496d916efe
dalismiles <dali.iapichino@gmail.com>
2022-07-22 17:45:14 +0200

    chore:first init

---
+++ src/assets/seriesMock.js
@@ -0,0 +1,60 @@
+const myList = [
+  {
+    id: "a0e2e984-f971-44cf-b033-cd49bfdb972b",
+    title: "Grace and Frankie",
+    seasons: 7,
+    poster:
+      "https://movieplayer.net-cdn.it/t/images/2015/04/10/grace_and_frankie_ver8_jpg_400x0_crop_q85.jpg",
+    year: 2015,
+    description:
+      "The series stars Jane Fonda and Lily Tomlin as the eponymous Grace Hanson and Frankie Bergstein, two aging women who form an unlikely friendship after their husbands reveal they are in love with each other and plan to get married.",
+    genres: ["comedy"],
+    new: true,
+    favorite: false,
+    rating: 5,
+    users: [2, 3],
+    cast: ["Jane Fonda", "Lily Tomlin", "Sam Waterston", "Martin Sheen"],
+  },
+
+  {
+    id: "a0e2e984-f971-44cf-b033-cd49bhcs972b",
+    title: "The Crown",
+    seasons: 4,
+    poster:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSdAjF4Q0XB16Y9wo7Vo1QV4UCAxaGnqCsmWOVMmIJsHrhjCUOf",
+    year: 2016,
+    description:
+      "The Crown is a historical drama television series about the reign of Queen Elizabeth II, created and principally written by Peter Morgan and produced by Left Bank Pictures and Sony Pictures Television for Netflix.",
+    genres: ["historical drama"],
+    new: true,
+    favorite: false,
+    rating: 5,
+    users: [2, 3],
+    cast: [
+      "Olivia Colman",
+      "Tobias Menzies",
+      "Helena Bonham Carter",
+      "Ben Daniels",
+      "Jason Watkins",
+    ],
+  },
+
+  {
+    id: "a0e2e984-f971-44cf-b033-sh51bhcs972b",
+    title: "Unorthodox",
+    seasons: 1,
+    poster:
+      "https://movieplayer.net-cdn.it/t/images/2020/04/01/unorthodox-poster_jpg_400x0_crop_q85.jpg",
+    year: 2016,
+    description:
+      "Esty, a 19-year-old Jewish woman, is living unhappily in an arranged marriage among the Satmar sect of the ultra-Orthodox community in Williamsburg, Brooklyn, New York City. She runs away to Berlin, where her estranged mother lives, and tries to navigate a secular life, discovering life outside her community and rejecting all of the beliefs she grew up with. Her husband, who learns that she is pregnant, travels to Berlin with his cousin, by order of their rabbi, to try to find her.",
+    genres: ["historical drama"],
+    new: true,
+    favorite: false,
+    rating: 5,
+    users: [2, 3],
+    cast: ["Shira Haas", "Amit Rahav", "Jeff Wilbusch"],
+  },
+];
+
+export default myList;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <h5 className="Footer__Title">
+        We're going to add new features soon! STAY TUNED!
+      </h5>
+    </div>
+  );
+};
+
+export default Footer;
